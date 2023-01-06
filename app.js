9815f28431044b5c935720d414af9c6f0c1b92a8
Paulo Alba <pr2013com@hotmail.com>
2023-01-06 10:35:48 -0300

    Update app.js

--- app.js
+++ app.js
@@ -18,24 +18,3 @@ https.createServer({
       key: fs.readFileSync("./SSL/code.key")
     }, app).listen(port, () => console.log("Rodando em https"))
 
-
-module.exports = {
-  trailingSlash: false,
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: '/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
-          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization' },
-        ],
-      },
-    ];
-  },
-  async redirects() {
-    return [];
-  }
-};
