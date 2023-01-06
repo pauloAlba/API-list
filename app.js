5f3506b346e60c9b888a9efee5ed96776edffe82
Paulo Alba <pr2013com@hotmail.com>
2023-01-05 23:49:40 -0300

    update para render

--- app.js
+++ app.js
@@ -4,18 +4,17 @@ import fs from "fs";
 import https from "https";
 import cors from "cors";
 
-const port = process.env.PORT || 3000
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(router);
 
-app.listen(3001, () => console.log("Servidor Rodando"));
+app.listen(3000, () => console.log("Servidor Rodando"));
 
 https.createServer({
       cert: fs.readFileSync("src/SSL/code.crt"),
       key: fs.readFileSync("src/SSL/code.key")
-    }, app).listen(port, () => console.log("Rodando em https"))
+    }, app).listen(3001, () => console.log("Rodando em https"))
 
 
 
