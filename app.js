02eaf7c2b6eac809ea1399296f6206ea92ab44ff
Paulo Alba <pr2013com@hotmail.com>
2023-01-06 09:58:05 -0300

    Update app.js

--- app.js
+++ app.js
@@ -14,6 +14,6 @@ const server = https.createServer(app)
 
 
 https.createServer({
-      cert: fs.readFileSync("src/SSL/code.crt"),
-      key: fs.readFileSync("src/SSL/code.key")
+      cert: fs.readFileSync("./SSL/code.crt"),
+      key: fs.readFileSync("./SSL/code.key")
     }, app).listen(port, () => console.log("Rodando em https"))
