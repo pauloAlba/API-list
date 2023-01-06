9f5e716d67f272deefde4a606b1ea70458e970aa
Paulo Alba <pr2013com@hotmail.com>
2023-01-06 10:47:29 -0300

    Update app.js

--- app.js
+++ app.js
@@ -9,12 +9,10 @@ app.use(express.json());
 app.use(cors());
 app.use(router);
 
-const port = process.env.PORT || 3000;
+const port = "4433"
 const server = https.createServer(app)
 
-
 https.createServer({
-      cert: fs.readFileSync("./SSL/code.crt"),
-      key: fs.readFileSync("./SSL/code.key")
-    }, app).listen(port, () => console.log("Rodando em https"))
-
+  cert: fs.readFileSync("src/SSL/code.crt"),
+  key: fs.readFileSync("src/SSL/code.key")
+}, app).listen(port, () => console.log("Rodando em https"))
